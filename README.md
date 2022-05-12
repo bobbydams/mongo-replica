92e43ab813949deecd3b66752a7b4345a975222a
Bobby Drake <bobby.drake@hal24k.com>
2022-05-12 18:16:30 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mongo-replica
+
 MongoDb ReplicaSet in Docker Compose
+
+## To start
+
+```shell
+$ ./start-db.sh
+```
+
+## To Stop
+
+```shell
+$ docker-compose down
+```
--- start-db.sh
+++ start-db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 sleep 5
 
 docker exec mongo-repl1 /scripts/rs-init.sh
+
+docker-compose logs --follow --tail=25
